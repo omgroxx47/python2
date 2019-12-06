bd9ca19812bc3d7acd3b42d35cad976a5c509c41
omgroxx47 <44902518+omgroxx47@users.noreply.github.com>
2019-12-06 07:55:04 +0530

    Add files via upload

---
+++ Kt exams C/divisionlogical.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main()
+{
+
+int m1, m2, m3, m4, m5, per;
+
+printf ("Enter marks in five subjects");
+scanf ("%d %d %d %d %d", &m1, &m2, &m3, &m4, &m5);
+
+
+per = (m1+m2+m3+m4+m5)/ 5 ;
+
+if (per>=60)
+printf ("First Division");
+
+if ((per>=50) && (per<60))
+
+printf ("Second Division");
+
+if ((per>=40) && (per<50))
+
+printf ("Third Division");
+
+if (per<40)
+
+printf ("Fail");
+
+}
